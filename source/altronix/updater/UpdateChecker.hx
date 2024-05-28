17d8177056ad90b84e9500131969b36b3cef42fd
AltronMaxX <max06112004@gmail.com>
2024-05-28 14:11:26 +0400

    better check for latest commit

--- source/altronix/updater/UpdateChecker.hx
+++ source/altronix/updater/UpdateChecker.hx
@@ -1,5 +1,6 @@
 package altronix.updater;
 
+import altronix.github.GitHub.GitHubRelease;
 import funkin.util.Constants;
 import haxe.Http;
 import haxe.Json;
@@ -17,17 +18,71 @@ class UpdateChecker
 
   static function getLatestCommitHash():String
   {
+    var repoCommits:Array<String> = parseCommits(sendRequest("https://api.github.com/repos/Altronix-Team/FNF-AltronixEngine/commits"));
+    var commit = getLatestReleaseCommit();
+    if (commit != repoCommits[0])
+    {
+      for (i in 0...repoCommits.length)
+      {
+        if (commit == repoCommits[i])
+        {
+          trace('The last release commit is lagging behind the last repository commit by $i commits');
+        }
+      }
+    }
+
+    return commit;
+  }
+
+  static function getLatestReleaseCommit():String
+  {
+    var releasesCommits:Array<String> = parseReleases(sendRequest("https://api.github.com/repos/Altronix-Team/FNF-AltronixEngine/releases"));
+    return Json.parse(sendRequest('https://api.github.com/repos/Altronix-Team/FNF-AltronixEngine/git/ref/tags/${releasesCommits[0]}'))?.object?.sha?.substr(0,
+      7);
+  }
+
+  static function parseCommits(rawJson:String):Array<String>
+  {
+    if (rawJson == "") return null;
+    var commits:Array<Dynamic> = Json.parse(rawJson);
+    var ret:Array<String> = [];
+
+    for (commit in commits)
+    {
+      var commitSha:String = commit.sha;
+      commitSha = commitSha.substr(0, 7);
+      ret.push(commitSha);
+    }
+    return ret;
+  }
+
+  static function parseReleases(rawJson:String):Array<String>
+  {
+    if (rawJson == "") return null;
+    var commits:Array<GitHubRelease> = Json.parse(rawJson);
+    var ret:Array<String> = [];
+
+    for (commit in commits)
+    {
+      var commitSha:String = commit.tag_name;
+      ret.push(commitSha);
+    }
+    return ret;
+  }
+
+  static function sendRequest(url:String):String
+  {
     var ret:String = "";
-    var http = new Http("https://api.github.com/repos/Altronix-Team/FNF-AltronixEngine/branches/develop");
+    var http = new Http(url);
     http.setHeader("User-Agent", "request");
     http.onData = function(data:String) {
-      var commit:String = Json.parse(data)?.commit?.sha;
-      ret = commit.substr(0, 7);
+      ret = data;
     }
     http.onError = function(msg:String) {
       throw "Error while getting upstream commit! Message: " + msg;
     }
     http.request(false);
+
     return ret;
   }
 }
